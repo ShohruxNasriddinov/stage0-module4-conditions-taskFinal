955498d8badada244932891558aa0a53135a2878
ShohruxNasriddinov <shohshoh1104@gmail.com>
2022-12-07 15:44:27 +0500

    cpmmit taskFinal

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,36 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if (year<0){
+            System.out.println("invalid date");
+        }
+        int a = year/100;
+        int b = a%4;
+        if (b==0 && year%4==0 && month==2){
+            System.out.println("29");
+        }else {
+            switch (month){
+                case 12:
+                case 10:
+                case 8:
+                case 7:
+                case 5:
+                case 3:
+                case 1:
+                    System.out.println("31");
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    System.out.println("30");
+                    break;
+                case 2:
+                    System.out.println("28");
+                    break;
+                default:
+                    System.out.println("invalid date");
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,29 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        switch (monthNumber){
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+            case 8:
+            case 7:
+                System.out.println("Summer");
+                break;
+            case 10:
+            case 9:
+            case 11:
+                System.out.println("autumn");
+                break;
+            default:
+                System.out.println("Wrong month number");
+        }
     }
 }
